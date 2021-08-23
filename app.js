1fe489645cff14689bbf4c1e3445baa8a7f412f4
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-23 17:30:09 +0100

    modified after routes and middlewares

--- app.js
+++ app.js
@@ -2,33 +2,37 @@ const express = require("express"),
     app = express(),
     mongoose = require("mongoose")
     port = 3000;
-
-// authRouter= require("./routers/auth");
-// userRouter = require("./routers/user");
+    app.use(express.json());
+ authRouter= require("./routers/auth");
+userRouter = require("./routers/user");
 // taskRouter = require("./routers/task");
 
-// app.use("/",authRouter);
-// app.use("/user",userRouter);
+app.use("/auth",authRouter);
+app.use("/user",userRouter);
 // app.use("/task",taskRouter);
 
-app.use(express.json());
+
 
 app.get('*', (req, res) => 
 {   //?404 PAGE
     res.status(404).send({error :"ERROR NOT FOUND"});
 
 });
-
+        //? Errors handler function
+        app.use(function (err, req, res, next) {
+            console.error(err);
+            res.status(err.status).json({ error: err.message });
+        });
 
 
 mongoose.set("debug", true); // in devolpment process
 mongoose
-    .connect( 'mongodb+srv://Admin:bvF3Y2JGlv3oqANc@cluster0.jipf0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
+    .connect( 'mongodb+srv://Admin:bvF3Y2JGlv3oqANc@cluster0.jipf0.mongodb.net/ToDo-API?retryWrites=true&w=majority' 
 				,{		
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-        dbName: "",
+        dbName: "ToDo-API",
     })
     .then((con) => {
         console.log("Database is connected");
