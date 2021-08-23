0765b119f6d4b43bc37e2f8b1cdf0d98bfa62a6a
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-23 17:27:30 +0100

    updated models

--- models/user.js
+++ models/user.js
@@ -11,13 +11,14 @@ bcrypt = require("bcrypt");
 let userSchema = new mongoose.Schema({
     username:
     {
-    type : String ,
-    required : true
+        type : String ,
+        required : true,
+        unique: true,
     },
     password :
     {
         type:String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
@@ -25,8 +26,38 @@ let userSchema = new mongoose.Schema({
         unique: true,
     },
     image:{
-        type:String
+        type:String,
     }
-
+    
 });
-module.exports = mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+    try {
+        if (this.isModified("password")) this.password = await bcrypt.hash(this.password, 13);
+        next();
+    } catch (e) {
+        next(e);
+    }
+    });
+    userSchema.methods.comparePasswords = async function (passwordSent, next) {
+    try {
+        return await bcrypt.compare(passwordSent, this.password);
+    } catch (e) {
+        next(e);
+    }
+};
+userSchema.methods.insertToken = function () {
+    let user = this.toObject();
+    delete user.password;
+    user.token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username,
+        },
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiWW91Y2VmIiwibGFzdE5hbWUiOiJNYWRhZGkifQ.Uv4mrLKQWkOjAps3m83Dle8YIU1wC37dFM3FNAKXugg",
+        {
+            expiresIn: "100h",
+        }
+    );
+    return user;
+    };
+module.exports = mongoose.model("User", userSchema);
