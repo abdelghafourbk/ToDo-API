39478edfe07a53c2d9ce279861be972a972ac66c
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-24 14:57:55 +0100

    aaa

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,15 @@
 const express = require("express"),
-    { updateUser} = require("../middlewares/user"),
+    { updateUser, logoutUser,deleteUser,uploadImage,deleteUserImage} = require("../middlewares/user"),
     {isLoggedIn} = require("../middlewares/auth");
     router = express.Router();
     router.route("/updateUser/:id").put(isLoggedIn,updateUser);
-    router.route("/showusers").get();
-    // createUser
+    router.route("/Logout/:id").get(isLoggedIn,logoutUser);
+    router.route("/deleteUser/:id").delete(isLoggedIn,deleteUser);
+    router.route("/uploadImage/:id").put(isLoggedIn,uploadImage);
+    router.route("/deleteUserImage/:id").delete(isLoggedIn,deleteUserImage);
 module.exports = router;
+
+
 
 
 
