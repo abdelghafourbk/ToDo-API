2dd10c294fdae25570d56a3bd14110a9c547e32b
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-24 14:58:49 +0100

    aaa

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,5 +1,4 @@
 const User = require("../models/user");
-
 module.exports = {
     createUser: async (req, res,next) => {
        
@@ -44,29 +43,58 @@ module.exports = {
             next({ message: e.message, status: 500 });
         }
     },
+    logoutUser: async (req, res, next)=>{
+        id= req.params.id;
+        try{
+            const user = await User.findById(id);
+            // maybe we will replace the token with an empty string and then make it expired
+            //after 1ms or make the old token expire directly but it's not supported with 
+            //jwt .
+            //we must delete the token here or make it expired after 1 ms or something
+             //res.json({loggout: "successfully"});
+             res.redirect('http://localhost:3000/auth/Login');
+        }catch(e){
+            next({ message: e.message, status: 500 });
+            }
+    },
 
-    // deleteUser: async(req, res, next) => {
-    //     const{username, password} = req.body,
-    //         id=req.params.id;
-    //     try{
-    //         if (!(id.toString()==(req.user._id).toString()))
-    //         return next({
-    //             message: "You aren't allowed to delete other Users.",
-    //             status: 401, 
-    //         });
-    //     const u = await User.findById(id);
-    //     u.username = username ? username : u.username;   
-    //     u.password = password ? password : u.password;
-    //     await u.delete();
-    // },
+    deleteUser: async(req,res,next)=>{
+            id = req.params.id;
+        try {
+            const u = await User.findById(id);
+            console.log(u.username);
+            await u.remove();
+            res.json({ deleted: "successfully" });
+        } catch (e) {
+            next({ message: e.message, status: 500 });
+        }
+    },
 
-    // deleteImage : async(req,res,next)=>
-    // {
+    uploadImage: async (req, res,next) => {
+        const { image } = req.body,
+            id = req.params.id;
+        try {
+            const u = await User.findById(id);
+            u.image = image ? image : u.image;   
+            await u.save();
+            res.status(201).json(u);
+        } catch (e) {
+            next({ message: e.message, status: 500 });
+        }
+    },
 
-        
-    // }
-    // ,
-
+    deleteUserImage: async(req,res,next)=>{
+        id = req.params.id;
+    try {
+        const u = await User.findById(id);
+         u.image="";
+        await u.save();
+        console.log(u.image);
+        res.json({  deleted: "successfully" });
+    } catch (e) {
+        next({ message: e.message, status: 500 });
+    }
+},
 
     
 
