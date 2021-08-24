647eae041475eee74c52d268115f77fd0f26ebb9
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-24 15:00:49 +0100

    aaa

--- routers/task.js
+++ routers/task.js
@@ -1,1 +1,6 @@
-//awdawd
+const express = require("express"),
+ {AddTask} = require("../middlewares/task"),
+{isLoggedIn} = require("../middlewares/auth");
+    router = express.Router();
+    router.route("/AddTask").post(isLoggedIn,AddTask);
+    module.exports = router;
