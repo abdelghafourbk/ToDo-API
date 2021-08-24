3f9fdcd08c4b2e47b5eb49803095332823de1961
abdelghafourbk <65504431+abdelghafourbk@users.noreply.github.com>
2021-08-24 14:59:47 +0100

    aaa

--- middlewares/task.js
+++ middlewares/task.js
@@ -1,7 +1,26 @@
 const Task = require("../models/task");
-model.exports = {
+module.exports = {
 
-   
+    AddTask: async(req,res,next)=>{
+        try {
+            const{user, Index, title, completed, day} = req.body;
+            task = await Task.create({user, Index, title, completed, day});
+            res.status(201).json(task);
+        } catch (e) {
+            next({ message: e.message, status: 500 });
+        }
+    },
+
+    /*ShowTasks:
+
+    ShowCompletedTask:
+
+    ShowSpecificTask:
+
+    UpdateTask:
+
+    DeleteTask:*/
+
 
     
 };
